29f32a640ee873e57f5ebb4d4f50bfbae397ca67
Joachim Thomassen <joachimt99@gmail.com>
2019-02-12 15:16:57 +0100

    Update

--- chairs_per_person.py
+++ chairs_per_person.py
@@ -1,6 +1,15 @@
-f = open("reservations.csv")
+try:
+    f = open("reservations.csv")
+except FileNotFoundError:
+    print("File must be of type .csv (comma separated values)")
+    f = open("reservations")
 
 for reservation in f:
     name, number = reservation.split(",")
-    chairs_per_person = 50 / int(number)
+    try:
+        chairs_per_person = 50 / int(number)
+    except ValueError:
+        print("Value must be numerical of integral type")
+    except ZeroDivisionError:
+        print("A booking cannot contain a value of 0 people")
     print("{} will get {} chairs per person".format(name, chairs_per_person))
